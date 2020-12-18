83e23e2948576b9e8aeac81924eb3c98c4be083d
WulfAlpha <drace.davidson@gmail.com>
2020-12-17 21:09:09 -0700

    added a bash script added solus alias list

---
+++ update.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "Ready to update..."
+echo "press enter to continue..."
+pause
+echo "before running me uncomment the proper command to use..."
+# sudo eopkg upgrade Solus
+# sudo apt update && sudo apt autoremove && sudo apt upgrade Deb/ubuntu
+# sudo pamac update Manjaro
+# sudo pacman -Syu Arch
+echo "Thanks for running this program!"
