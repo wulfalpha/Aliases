aeb09397ef993fdabec7623d615c9d5ef6f2e21b
wulfalpha <drace.davidson@gmail.com>
2021-02-22 17:32:47 -0700

    added piConnect. A file to connect to your pi! Just add the ip address.

---
+++ piConnect.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+echo "What is your ip address"
+
+read ipaddr
+
+echo "Connecting to Raspberry Pi"
+
+# if you have Mosh installed (recommended) uncomment the mosh version. Otherwise uncomment the ssh version.
+# ssh version
+# ssh pi@ipaddr
+
+# mosh version
+# mosh pi@$ipaddr
