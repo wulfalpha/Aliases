a341c0adb60d22fd9dc8c96e3e0e36040c3f12fc
wulfalpha <drace.davidson@gmail.com>
2021-08-22 19:19:43 -0600

    fresh pi with love!

--- piConnect.sh
+++ piConnect.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
-echo "What is your ip address"
+# Created by WulfAlpha with love!
+echo "What is your ip address?"
 
 read -r ipaddr
 
-echo "Connecting to Raspberry Pi"
+echo "Connecting to Raspberry Pi..."
 
 # if you have Mosh installed (recommended) uncomment the mosh version. Otherwise uncomment the ssh version.
 # ssh version
 # ssh pi@ipaddr
-
+clear && pwd
 mosh version
-mosh pi@$ipaddr
+mosh pi@"$ipaddr"
