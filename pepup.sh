a2661498e80ed5b16aa11f6e0d64729477deda66
Wulfalpha <drace.davidson@gmail.com>
2022-03-02 14:18:00 -0700

    added if statment to prevent hangs when run

--- pepup.sh
+++ pepup.sh
@@ -4,4 +4,6 @@ echo "Updating System..."
 sudo apt update && sudo apt autoremove --yes && sudo apt upgrade --yes && sudo apt autoclean
 notify-send 'Update Complete!'
 echo "Thanks for using this program!"
-read -nl -r -p "Press enter to continue..."
+if [ $TERM = “xterm-256color” ]; then
+    read -nl -r -p "Press enter to continue..."
+fi
