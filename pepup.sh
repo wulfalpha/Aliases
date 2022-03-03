3f24296f082cac3bd52dac1dd1afa53dcc7ecc6a
Wulfalpha <drace.davidson@gmail.com>
2022-03-02 21:21:45 -0700

    spacing

--- pepup.sh
+++ pepup.sh
@@ -2,7 +2,7 @@
 clear
 echo "Updating System..."
 sudo apt update && sudo apt autoremove --yes && sudo apt upgrade --yes && sudo apt autoclean
-notify-send 'Update Complete!'
+notify-send '📦  Update Complete!'
 echo "Thanks for using this program!"
 if [ $TERM = "xterm-256color" ]; then
     read -nl -r -p "Press enter to continue..."
