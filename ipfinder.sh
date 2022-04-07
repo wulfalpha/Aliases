b97a2cfb4e35278563bb0cb7964343e4f7d84bc3
Wulfalpha <drace.davidson@gmail.com>
2022-04-06 22:42:39 -0600

    fixed to show actual ip address

--- ipfinder.sh
+++ ipfinder.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 clear
-ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/'
+ip -4 a | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/'
