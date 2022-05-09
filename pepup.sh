46a381324665633bddf7d17eaee7e3bd8bf48a3b
Wulfalpha <drace.davidson@gmail.com>
2022-05-09 17:11:35 -0600

    Updated to Latest Version. YAY!!!!

--- pepup.sh
+++ pepup.sh
@@ -4,6 +4,4 @@ echo "Updating System..."
 sudo apt update && sudo apt autoremove --yes && sudo apt upgrade --yes && sudo apt autoclean
 notify-send 'Update Complete!'
 echo "Thanks for using this program!"
-if [ $TERM = "xterm-256color" ]; then
-    read -nl -r -p "Press enter to continue..."
-fi
+read -n1 -r -p "Press enter to continue..."
