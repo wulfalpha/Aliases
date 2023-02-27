e48c9ae77fcc18128814dcc1bf3a50c90edd0285
Andrew Davidson <wulfalpha@peperwulf>
2023-02-27 12:42:23 -0700

    added new servers

--- nordgtk.py
+++ nordgtk.py
@@ -18,10 +18,7 @@ class NordWindow(Gtk.Window):
         frame1 = Gtk.Frame(label="Nord Select")
         nord = sub.check_output(["nordvpn", "status"]).decode()
         switch = False
-        for line in nord.split():
-            if "Disconnected" in line:
-                switch = True
-        if switch:
+        if "Disconnected" in nord:
             sub.call(["./notimin.sh"])
         else:
             sub.call(["./notifyier.sh"])
@@ -31,25 +28,42 @@ class NordWindow(Gtk.Window):
         label1 = Gtk.Label(label="Select a server Below.")
         label1.set_hexpand(True)
 
+        if "Connected" in nord:
+            status = "Connected"
+        else:
+            status = "Disconnected"
+
         label2 = Gtk.Label()
-        self.label3 = Gtk.Label("None")
+        self.label3 = Gtk.Label(status)
         self.label3.set_hexpand(True)
 
-        button_us = Gtk.Button(label="United States")
+        button_us = Gtk.Button(label="🇺🇸")
         button_us.set_hexpand(True)
         button_us.connect("clicked", self.on_button_us_clicked)
 
 
-        button_uk = Gtk.Button(label="United Kingdom")
+        button_uk = Gtk.Button(label="🇬🇧")
         button_uk.set_hexpand(True)
         button_uk.connect("clicked", self.on_button_uk_clicked)
 
 
-        button_jp = Gtk.Button(label="Japan")
+        button_jp = Gtk.Button(label="🇯🇵")
         button_jp.set_hexpand(True)
         button_jp.connect("clicked", self.on_button_jp_clicked)
 
-        button_ex = Gtk.Button(label="Disconnect")
+        button_mx = Gtk.Button(label="🇲🇽")
+        button_mx.set_hexpand(True)
+        button_mx.connect("clicked", self.on_button_mx_clicked)
+
+        button_ca = Gtk.Button(label="🇨🇦")
+        button_ca.set_hexpand(True)
+        button_ca.connect("clicked", self.on_button_ca_clicked)
+
+        button_fn = Gtk.Button(label="🇫🇮")
+        button_fn.set_hexpand(True)
+        button_fn.connect("clicked", self.on_button_fn_clicked)
+
+        button_ex = Gtk.Button(label="⏼")
         button_ex.set_hexpand(True)
         button_ex.connect("clicked", self.on_button_ex_clicked)
 
@@ -63,8 +77,11 @@ class NordWindow(Gtk.Window):
         grid1.attach(button_us, 0, 7, 1, 1)
         grid1.attach(button_uk, 1, 7, 1, 1)
         grid1.attach(button_jp, 2, 7, 1, 1)
-        grid1.attach(button_ex, 0, 8, 1, 1)
-        grid1.attach(button_q, 2, 8, 1, 1)
+        grid1.attach(button_mx, 0, 8, 1, 1)
+        grid1.attach(button_ca, 1, 8, 1, 1)
+        grid1.attach(button_fn, 2, 8, 1, 1)
+        grid1.attach(button_ex, 0, 9, 1, 1)
+        grid1.attach(button_q, 2, 9, 1, 1)
 
         self.add(frame1)
         frame1.add(grid1)
@@ -73,33 +90,54 @@ class NordWindow(Gtk.Window):
     def on_button_us_clicked(self, widget):
         sub.Popen("nordvpn connect us", shell=True)
         self.label3.set_text("US")
-        sleep(2)
+        sleep(1.5)
         sub.call(["./notifyier.sh"])
 
 
     def on_button_uk_clicked(self, widget):
         sub.Popen("nordvpn connect uk", shell=True)
         self.label3.set_text("UK")
-        sleep(2)
+        sleep(1.5)
         sub.call(["./notifyier.sh"])
+
 
     def on_button_jp_clicked(self, widget):
         sub.Popen("nordvpn connect jp", shell=True)
-        self.label3.set_text("JP")
-        sleep(2)
+        self.label3.set_text("Japan")
+        sleep(1.5)
+        sub.call(["./notifyier.sh"])
+
+
+    def on_button_mx_clicked(self, widget):
+        sub.Popen("nordvpn connect mexico", shell=True)
+        self.label3.set_text("Mexico")
+        sleep(1.5)
+        sub.call(["./notifyier.sh"])
+
+
+    def on_button_ca_clicked(self, widget):
+        sub.Popen("nordvpn connect ca", shell=True)
+        self.label3.set_text("Canada")
+        sleep(1.5)
+        sub.call(["./notifyier.sh"])
+
+
+    def on_button_fn_clicked(self, widget):
+        sub.Popen("nordvpn connect finland", shell=True)
+        self.label3.set_text("Finland")
+        sleep(1.5)
         sub.call(["./notifyier.sh"])
 
 
     def on_button_ex_clicked(self,widget):
         sub.Popen("nordvpn d", shell=True)
         self.label3.set_text("Disconnected")
-        sleep(2)
+        sleep(1)
         sub.call(["./notimin.sh"])
 
 
-win1 = NordWindow()
-
-win1.connect("destroy", Gtk.main_quit)
-
-win1.show_all()
-Gtk.main()
+if __name__ == __main__:
+    win1 = NordWindow()
+    win1.connect("destroy", Gtk.main_quit)
+    win1.show_all()
+    Gtk.main()
