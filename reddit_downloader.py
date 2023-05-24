5550864499a29256281a8f1c632426987808962b
Andrew <drace.davidson@gmail.com>
2023-05-23 18:11:58 -0600

    added sucessfully gtk spinner to reddit_downloader

--- reddit_downloader.py
+++ reddit_downloader.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 import gi
+
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
-import subprocess as s
-from functools import partial
-from subprocess import call
+from gi.repository import Gtk, GLib
+from subprocess import run
 import shlex
+import threading
 
 
 class RedditWindow(Gtk.Window):
     """Window update class."""
+
     def __init__(self):
         super().__init__(title="Reddit Downloader (GTK)")
 
@@ -19,26 +20,24 @@ class RedditWindow(Gtk.Window):
         self.set_resizable(True)
 
         frame1 = Gtk.Frame(label="Reddit Downloader")
+        grid1 = Gtk.Grid(row_spacing=10, column_spacing=10, column_homogeneous=True)
 
-        grid1 = Gtk.Grid(row_spacing = 10, column_spacing = 10, column_homogeneous = True)
+        self.status_label = Gtk.Label(label="Status: Ready...")
+        self.status_label.set_hexpand(True)
 
-        label1 = Gtk.Label(label="Status:")
-        label1.set_hexpand(True)
-
-        self.label2 = Gtk.Label(label="Ready...")
-        self.label2.set_hexpand(True)
-        self.label2.set_vexpand(True)
-
+        self.spinner = Gtk.Spinner()
+        self.spinner.set_hexpand(True)
+        self.spinner.set_vexpand(True)
 
         button_fetch_reddits = Gtk.Button(label="Get Images")
         button_fetch_reddits.set_hexpand(True)
         button_fetch_reddits.connect("clicked", self.on_button_fetch_reddits_clicked)
         button_fetch_reddits.set_tooltip_text("Reddit")
 
-        reddit_list = Gtk.ListStore(int,str)
-        reddit_list.append([1,"reddits.txt"])
-        reddit_list.append([2,"moe.txt"])
-        reddit_list.append([3,"imaginary.txt"])
+        reddit_list = Gtk.ListStore(int, str)
+        reddit_list.append([1, "reddits.txt"])
+        reddit_list.append([2, "moe.txt"])
+        reddit_list.append([3, "imaginary.txt"])
 
         source_combo = Gtk.ComboBox.new_with_model_and_entry(reddit_list)
         source_combo.set_entry_text_column(1)
@@ -51,8 +50,8 @@ class RedditWindow(Gtk.Window):
         button_q.connect("clicked", Gtk.main_quit)
         button_q.set_tooltip_text("Quit")
 
-        grid1.attach(label1, 0, 2, 3, 2)
-        grid1.attach(self.label2, 0, 4, 3, 2)
+        grid1.attach(self.status_label, 0, 2, 3, 2)
+        grid1.attach(self.spinner, 0, 4, 3, 2)
         grid1.attach(button_fetch_reddits, 0, 8, 1, 1)
         grid1.attach(source_combo, 1, 8, 1, 1)
         grid1.attach(button_q, 2, 8, 1, 1)
@@ -60,14 +59,13 @@ class RedditWindow(Gtk.Window):
         self.add(frame1)
         frame1.add(grid1)
 
-
     def on_button_fetch_reddits_clicked(self, widget):
         """Button to fetch reddits"""
-        self.label2.set_text("Downloading...")
-        fetch_reddit(win1)
-        #s.run('./reddit_reader.sh', shell=True)
-        self.label2.set_text("Done.")
+        self.spinner.start()
 
+        # Create and start the worker thread
+        thread = threading.Thread(target=self.fetch_reddit)
+        thread.start()
 
     def on_source_combo_changed(self, combo):
         """Combo box V1: An attempt to let the user select the reddit list to pull from"""
@@ -81,15 +79,26 @@ class RedditWindow(Gtk.Window):
             reddit = entry.get_text()
         return reddit
 
+    def update_status_label(self, text):
+        """Update the status label in the main GTK thread"""
+        GLib.idle_add(self.status_label.set_text, "Status: " + text)
 
-def fetch_reddit(win1):
-    """Method to fetch reddits from list."""
-    count = 10
-    file_path = "/home/wulfalpha/Pictures/reddits"
-    reddit_list = win1.on_source_combo_changed(win1.combo)
-    with open(reddit_list, "r") as stream:
-        for reddit in stream:
-            call(shlex.split(f"bdfr download -s {reddit} -L {count} {file_path}"))
+    def fetch_reddit(self):
+        """Method to fetch reddits from list."""
+        count = 10
+        file_path = "/home/wulfalpha/Pictures/reddits"
+        reddit_list = self.on_source_combo_changed(self.combo)
+        with open(reddit_list, "r") as stream:
+            total_reddits = sum(1 for _ in stream)  # Count total number of reddits
+            stream.seek(0)  # Reset file pointer to the beginning
+            for i, reddit in enumerate(stream, start=1):
+                reddit = reddit.strip()
+                self.update_status_label(f"Downloading {reddit} ({i}/{total_reddits})")
+                # Perform the download using bdfr here
+                bdfr_command = f"bdfr download -s {reddit} -L {count} {file_path}"
+                run(shlex.split(bdfr_command))
+        self.update_status_label("Ready...")
+        self.spinner.stop()
 
 
 win1 = RedditWindow()
