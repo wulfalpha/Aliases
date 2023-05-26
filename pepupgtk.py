0e90280f56a5fa1623d595730c4a59c7364314be
Andrew Davidson <wulfalpha@peperwulf>
2023-05-26 13:40:04 -0600

    reverted accidental merge

--- pepupgtk.py
+++ pepupgtk.py
@@ -5,7 +5,6 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 import subprocess as s
 from functools import partial
-import apt
 
 
 class PepUpWindow(Gtk.Window):
@@ -55,20 +54,13 @@ class PepUpWindow(Gtk.Window):
         self.add(frame1)
         frame1.add(grid1)
 
+
     def on_button_updates_clicked(self, widget):
         """Button to check for updates"""
         self.spinner.start()
         self.label1.set_text("Updates: Checking...")
         s.run("apt-get -q update", shell=True)
-        updates = (
-            s.run(
-                "apt-get -q -y --ignore-hold --allow-change-held-packages --allow-unauthenticated -s dist-upgrade | /bin/grep  ^Inst | wc -l",
-                shell=True,
-                stdout=s.PIPE,
-            )
-            .stdout.decode("utf-8")
-            .strip()
-        )
+        updates = s.run("apt-get -q -y --ignore-hold --allow-change-held-packages --allow-unauthenticated -s dist-upgrade | /bin/grep  ^Inst | wc -l", shell=True, stdout=s.PIPE).stdout.decode("utf-8").strip()
         try:
             updates = int(updates)
         except ValueError:
