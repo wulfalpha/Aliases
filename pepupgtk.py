0b088ee16ab6877ae65c2397b7c53e21ace95d84
Andrew <drace.davidson@gmail.com>
2023-05-27 21:50:46 -0600

    re aranged imports

--- pepupgtk.py
+++ pepupgtk.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import gi
 import subprocess as s
-import threading
-from gi.repository import GLib, Gtk
 
 gi.require_version("Gtk", "3.0")
+import threading
+from gi.repository import GLib, Gtk
 
 
 class PepUpWindow(Gtk.Window):
