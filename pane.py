d82f6ecc4931625799ca12f197ffe0b6dfec721b
wulfalpha <drace.davidson@gmail.com>
2024-09-12 20:39:41 -0600

    updated pane

--- pane.py
+++ pane.py
@@ -1,42 +1,101 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
 import sys
-from PyQt5.QtCore import QUrl, QSize, Qt
-from PyQt5.QtGui import QIcon, QDesktopServices
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget
 import argparse
+import mpv
+import re
+from PyQt5.QtCore import QUrl, QSize, Qt
+from PyQt5.QtGui import QIcon, QResizeEvent
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings, QWebEngineDownloadItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QMessageBox, QSizePolicy, QFileDialog
+
+DEFAULT_URL = "https://search.brave.com"
+
+class PaneWebEngineView(QWebEngineView):
+    def __init__(self):
+        super().__init__()
+        self.settings().setAttribute(QWebEngineSettings.FullScreenSupportEnabled, True)
+
+        # Connect download requests to a slot
+        self.page().profile().downloadRequested.connect(self.handle_download_request)
+
+    def load_url(self, url):
+        if QUrl.fromUserInput(url).isValid():
+            self.load(QUrl(url))
+        else:
+            QMessageBox.critical(self, "Error", f"Invalid URL: {url}")
+
+    def handle_download_request(self, download):
+        # Prompt user to select a file location
+        save_path, _ = QFileDialog.getSaveFileName(self, "Save File", download.downloadFileName())
+        if save_path:
+            download.setPath(save_path)
+            download.accept()
+        else:
+            download.cancel()
+
 
 class Pane(QMainWindow):
-    """Class to build the Site specific Browser"""
-    def __init__(self, qrl):
-        super(Pane, self).__init__()
+    def __init__(self, url):
+        super().__init__()
 
-        # Get the screen resolution and calculate a comfortable window size
-        screen_size = QDesktopWidget().availableGeometry().size()
-        window_size = QSize(int(screen_size.width() * 0.8), int(screen_size.height() * 0.8))
+        # Create an MPV player instance
+        self.mpv_player = None
 
-        self.setMinimumSize(window_size)
-        self.setWindowIcon(QIcon('hidive.png'))
-        self.setWindowTitle(f"{qrl} - Pane")
-
-        self.browser = QWebEngineView()
-        try:
-            self.browser.load(QUrl(qrl))
-        except Exception as e:
-            print(e)
-
-        self.setCentralWidget(self.browser)
+        # Check if the URL points to a video
+        if self.is_video_url(url):
+            self.init_mpv(url)
+        else:
+            self.init_ui(url)
         self.show()
 
+    def init_ui(self, url):
+        screen_size = QDesktopWidget().availableGeometry().size()
+        window_size = QSize(int(screen_size.width() * 0.8), int(screen_size.height() * 0.8))
+        self.setMinimumSize(window_size)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-l", "--link", default="https://search.brave.com", help="hyperlink to access")
+        icon_path = "hidive.png"
+        if QIcon.hasThemeIcon(icon_path):
+            self.setWindowIcon(QIcon(icon_path))
+        else:
+            self.setWindowIcon(QIcon.fromTheme("application-x-executable"))
+
+        self.setWindowTitle(f"{url} - Pane")
+
+        self.browser = PaneWebEngineView()
+        self.browser.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.browser.load_url(url)
+        self.setCentralWidget(self.browser)
+
+    def init_mpv(self, video_url):
+        # Initialize MPV player
+        self.mpv_player = mpv.MPV(wid=str(int(self.winId())))
+        self.mpv_player.play(video_url)
+
+    def is_video_url(self, url):
+        # Simple regex to detect common video file formats and URLs
+        video_formats = r'.*\.(mp4|mkv|webm|avi|mov|flv)$'
+        return re.match(video_formats, url, re.IGNORECASE)
+
+    def resizeEvent(self, event: QResizeEvent):
+        if self.mpv_player:
+            self.mpv_player.command('set_property', 'wid', str(int(self.winId())))
+        else:
+            self.browser.resize(event.size())
+        super().resizeEvent(event)
+
+    def closeEvent(self, event):
+        if self.mpv_player:
+            self.mpv_player.terminate()  # Ensure MPV is closed properly
+        event.accept()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Site-Specific Browser")
+    parser.add_argument("-l", "--link", default=DEFAULT_URL, help="URL to open")
     args = parser.parse_args()
-    qrl = args.link
 
     app = QApplication(sys.argv)
-    app.setApplicationName(f"{qrl} - Pane")
-    window = Pane(qrl)
+    app.setApplicationName("Pane Browser")
+    window = Pane(args.link)
 
     app.exec_()
--- pepup.sh
+++ pepup.sh
@@ -1,7 +1,170 @@
 #! /bin/bash
-clear
-echo "Updating System..."
-sudo apt update && sudo apt autoremove --yes && sudo apt upgrade --yes && sudo apt autoclean
+# by: wulfAlpha
+################################################################ Disclaimer ###
+# These scripts come without warranty of any kind. Use them at your own risk. I
+# assume no liability for the accuracy, correctness, completeness, or usefulness
+# of any information provided by this script nor for any sort of damages using
+# these scripts may cause.
+#
+# pepup is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# pepup is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+########################################################## Init ###
+name="System_Update"
+dt="$(date)"
+Update_log=$name".md"
+
+# If exist a previews update file, delete it
+if [[ -f ~/$Update_log ]]
+  then rm ~/$Update_log
+fi
+# Create a new log file
+touch ~/$Update_log
+########################################### Colors ###
+# BackGround with setab
+bred=`tput setab 1`	# red
+# ForeGrownd with setaf
+red=`tput setaf 1`	# red
+grn=`tput setaf 2`	# yellow
+wht=`tput setaf 7`	# white
+# TxtFormat with setaf
+bld=`tput bold`    # Select bold mode
+# DefautMode with setaf
+reset=`tput sgr0`
+# Hide/show cursor
+nshw() { tput civis    # Hide cursor
+}
+yshw() { tput cnorm # return cursor to normal
+}
+# Peppermint Color
+Peppermint_Color=${bred}${bld}${wht}
+Peppermint_msg=${bld}${bred}${wht}
+Peppermint_good=${bld}${red}
+Peppermint_note=${bld}${red}
+Peppermint_err=${bld}${bred}${wht}
+################################################################## Text ###
+title() { # ≡ message
+printf "${Peppermint_Color} %s${reset}\n" "$@"
+}
+msg() { # ⓘ message
+printf "${Peppermint_msg} ⁖ ${reset}${bld}%s \n${reset}" "$@"
+}
+finished() {
+if [[ "$*" == "" ]];
+  then printf " ${Peppermint_good}[ ☑ COMPLETED ] ${reset}\n";
+else
+  printf " ${Peppermint_good}[ ☑ %s ] ${reset}\n" "$@"
+fi
+}
+note() { # [ NOTES ] message
+printf "${Peppermint_note} > ${reset} %s \n" "$@"
+}
+notice() { # [ ⚠ message ]
+if [ "$*" == "" ];
+  then printf "${Peppermint_err} [ ⚠ WARNING ] ${reset}\n"
+fi
+printf "${Peppermint_err} [ ⚡ %s ] ${reset}\n" "$@"
+}
+############################################################ Root ###
+is_root() { # I am Root!
+if [ "$(id -u)" -ne 0 ]; then sudo ls >/dev/null; fi
+}
+######################################################### Updating functions ###
+get() { # get updates
+sudo apt-get -y update >> ~/"$Update_log"
+}
+up() { # install
+sudo apt-get -y upgrade >> ~/"$Update_log"
+}
+housekeeping() { # clean up
+  sudo apt-get -y autoremove >> ~/"$Update_log"
+  sudo apt-get -y autoclean >> ~/"$Update_log"
+}
+############################################################ Progress Bar +  ###
+spin() {
+##  SYNTAX: spin "COMMAND" " Message" "Name of the job"
+##          spin "third action" "action" "3rd action"
+  nshw # hide cursor
+
+  # LP="\e[2K"
+  # Spinner Character
+  SPINNER="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
+
+  spinner() { # start
+    task=$1
+    msgs=$2
+    while :; do
+      jobs %1 > /dev/null 2>&1
+      [ $? = 0 ] || {
+       # printf "${LP}✓ ${task} Done"
+       finished "$task Done"
+        break
+      }
+      for (( i=0; i<${#SPINNER}; i++ )); do
+        sleep 0.05
+        printf "${bld}${red}${SPINNER:$i:1}${reset} ${Peppermint_msg} ${task} ${msgs}${reset}\r"
+      done
+    done
+  }
+
+  msgs="${2-InProgress}"
+  task="${3-$1}"
+  $1 & spinner "$task" "$msg"
+
+  yshw  # return cursor
+}
+
+############################################################## System Update ###
+pepup() {
+#  mupdate=`sudo apt-get update -y`
+  title " ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Pepermint Update ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  "
+        echo "$dt"
+        echo
+        echo "# System Update" > ~/"$Update_log"
+        echo "**"$Update_log"**" $dt >> ~/"$Update_log"
+  msg " Checking for Updates..."
+        echo "## Checking for Updates" >> ~/"$Update_log"
+  notice "... this may take some time ..."
+  spin 'get' " checking... " "Dowloading"
+  msg " Installing new updates..."
+        echo "## Instaling new Updates..." >> ~/"$Update_log"
+  notice "... please be wait ..."
+  spin "up" " updates... " "Installing"
+  msg " ... Cleaning up ..."
+        echo "## Final run..." >> ~/"$Update_log"
+        echo "*... Cleaning up ...*" >> ~/"$Update_log"
+  spin "housekeeping" " removing un-needed programs... " "Cleaning up... "
+        echo "[END]" >> ~/"$Update_log"
+}
+
+final() {
+#clear
+title " ▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞ Finished Updating! ▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚  "
+echo
+echo "For more information see the update log file:"
 notify-send 'Update Complete!'
-echo "Thanks for using this program!"
-read -n1 -r -p "Press enter to continue..."
+echo "${bld}>${grn} $Update_log${reset} located in your home directory"
+echo
+#read -p "Press [Enter] key to exit..."
+read -n1 -r -p " Press ${bld}\"Q\"${reset} to ${bld}Q${reset}uit, or $bld\"L\"$reset to view the log file. " pause
+}
+
+view_log() {
+[ "$pause" = "l" ] || [ "$pause" = "L" ] && ( clear
+ echo -e "$(cat ~/${Update_log})\n\n\t Press ${bld}\"Q\"${reset} to ${bld}Q${reset}uit and close this window. " | less -R
+ clear
+ ) || clear
+}
+
+clear
+is_root
+pepup
+final
+view_log
