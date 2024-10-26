37162361d299740379248f7374a81cb69dfd6845
wulfalpha <drace.davidson@gmail.com>
2024-10-25 21:19:27 -0600

    fixed pyspeak

--- pyspeak.py
+++ pyspeak.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
+"""
+Pulls pop culture sayings from a saying.txt file. takes nothing returns nothing.
+"""
+
 from random import choice
 
 worb_list = []
-with open("/home/wulfalpha/sayings.txt", "r") as db:
+with open("/home/wulfalpha/sayings.txt", "r", encoding="utf-8") as db:
     for line in db:
         worb_list.append(line.strip())
 
